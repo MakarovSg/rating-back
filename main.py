7dd8a76429e3a5f1dcd799b8197d731d9c9b4f4a
MakarovSg <sergey.makarov@chainstack.com>
2023-04-16 00:18:11 +0400

    Extend metrics for NFT and for age

--- main.py
+++ main.py
@@ -37,27 +37,31 @@ def fetch_all_nfts(base_url, page_size=1000):
 
 
 
-async def older_that_100_days(wallet_address):
+async def get_age_score(wallet_address):
     url = f"https://api.etherscan.io/api?module=account&action=txlist&address={wallet_address}&startblock=0&endblock=99999999&sort=asc&apikey={ETHERSCAN_API_KEY}"
     response = requests.get(url)
 
     if response.status_code != 200:
-        return False
+        return 0
 
     data = response.json()
     if data["status"] != "1":
         print("Failed to fetch transactions: " + data["message"])
-        return False
+        return 0
 
     transactions = data["result"]
 
     if not transactions:
-        return None
+        return 0
 
     oldest_transaction = transactions[0]
     timestamp = int(oldest_transaction["timeStamp"])
     age = datetime.datetime.now() - datetime.datetime.fromtimestamp(timestamp)
-    return age.days > 100
+    if age.days > 365:
+        return 5
+    elif age.days > 100:
+        return 2
+    return 0 
 
 
 def get_provider():
@@ -66,21 +70,23 @@ def get_provider():
 
 async def has_bluechip(address: str):
     w3 = Web3(get_provider())
-    collection_contract_addresses = [
-        "0x57f1887a8bf19b14fc0df6fd9b2acc9af147ea85".lower(),  # ENS
-        "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D".lower(),  # BAYC
-        "0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB".lower(),  # Punks
-        "0x60E4d786628Fea6478F785A6d7e704777c86a7c6".lower(),  # MAYC
-        "0xED5AF388653567Af2F388E6224dC7C4b3241C544".lower(),  # Azuki
-    ]
+    score = 0;
+    collection_contract_addresses = {
+        "0x57f1887a8bf19b14fc0df6fd9b2acc9af147ea85".lower(): 10,  # ENS
+        "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D".lower(): 6,  # BAYC
+        "0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB".lower(): 7,  # Punks
+        "0x60E4d786628Fea6478F785A6d7e704777c86a7c6".lower(): 4,  # MAYC
+        "0xED5AF388653567Af2F388E6224dC7C4b3241C544".lower(): 4,  # Azuki
+    }
     if not w3.is_address(address):
         raise ValueError("Invalid Ethereum address")
     nft_api_url = f"{ALCHEMY_BASE_URL}{ALCHEMY_API_KEY}/getNFTs?owner={address}&withMetadata=false"
     nft_data = fetch_all_nfts(nft_api_url)
     for nft in nft_data:
         nft_contract_address = nft["contract"]["address"]
-        if nft_contract_address.lower() in collection_contract_addresses:
-            return True
+        if nft_contract_address.lower() in collection_contract_addresses.keys():
+            score += collection_contract_addresses.pop(nft_contract_address.lower())
+            
 
     return False
 
@@ -99,11 +105,9 @@ async def get_score(address: str):
     balance_eth = balance_wei / 10**18
 
     rating = 1
-    max = 14
-    if await has_bluechip(address=address):
-        rating += 10
-    if await older_that_100_days(address):
-        rating += 2
+    max = 38
+    rating += await has_bluechip(address=address)
+    rating += await get_age_score(address)
     if balance_eth > 1:
         rating += 1
     response = {"rating": rating / max}
